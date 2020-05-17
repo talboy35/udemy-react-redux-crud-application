b81475fabc504e97c1e59a00c9e3d9a5039c03ab
talboy35 <pistatio0001@gmail.com>
2020-05-17 14:02:23 +0900

    change tag div to h1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="App-title">Hello React</h1>
-    </div>
-  );
+  return <h1>Hello React</h1>;
 }
 
 export default App;
